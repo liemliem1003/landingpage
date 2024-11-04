70da75eecc75e34505b0bb2795aeaf4e9bdc514c
Kayn <liem14021998@gmail.com>
2024-11-04 18:51:42 +0700

    04/11/2024 v3

---
+++ script.js
@@ -0,0 +1,37 @@
+function GetDownloadNumber() {
+    fetch('http://14.225.206.203:8080/api/v1/download/count')  
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok' + response.statusText);
+            }
+            return response.json();  
+        })
+        .then(data => {
+            console.log(data);
+            document.getElementById("downloadNumber").innerHTML = "Download: " + data
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+}
+
+GetDownloadNumber()
+
+function IncreaseNumberOfDownload(){
+    fetch('http://14.225.206.203:8080/api/v1/download/increment', {
+        method: 'POST', 
+        headers: {
+          'Content-Type': 'application/json'  
+        },
+        body: JSON.stringify({
+          
+        })
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log('Success:', data);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+}
