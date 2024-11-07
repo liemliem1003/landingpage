21451b6451cf49a169dadad35f521e115cba0305
Kayn <liem14021998@gmail.com>
2024-11-07 18:13:26 +0700

    07/11/2024

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function GetDownloadNumber() {
-    fetch('http://14.225.206.203:8080/api/v1/download/count')  
+    fetch('https://lequocanh.com:8443/api/v1/download/count')  
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok' + response.statusText);
@@ -18,7 +18,7 @@ function GetDownloadNumber() {
 GetDownloadNumber()
 
 function IncreaseNumberOfDownload(){
-    fetch('http://14.225.206.203:8080/api/v1/download/increment', {
+    fetch('https://lequocanh.com:8443/api/v1/download/increment', {
         method: 'POST', 
         headers: {
           'Content-Type': 'application/json'  
